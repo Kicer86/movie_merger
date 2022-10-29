fea4dc70e950dc50ded34acc664be843e7a0ab0c
Michał Walenciak <Kicer86@gmail.com>
2022-10-29 14:25:39 +0200

    Extract scene change frames from both files to temp dir

--- vof.py
+++ vof.py
@@ -1,6 +1,8 @@
 
+import os
 import sys
 import subprocess
+import tempfile
 
 
 if len(sys.argv) != 3:
@@ -10,5 +12,10 @@ if len(sys.argv) != 3:
 file1 = sys.argv[1]
 file2 = sys.argv[2]
 
+temp_location = tempfile.gettempdir() + "/VOF/" + str(os.getpid()) + "/"
+os.makedirs(name = temp_location)
+
 subprocess.run(["ffmpeg", "-i", file1, "-filter:v", "select=gt(scene\,0.4),showinfo", "-fps_mode",
-                "passthrough", "frames/%05d.jpg"])
+                "passthrough", temp_location + "lhs%05d.jpg"])
+subprocess.run(["ffmpeg", "-i", file2, "-filter:v", "select=gt(scene\,0.4),showinfo", "-fps_mode",
+                "passthrough", temp_location + "rhs%05d.jpg"])
