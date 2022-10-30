b030150d510b43a31632fd9102c7bda567ecb9b5
Michał Walenciak <Kicer86@gmail.com>
2022-10-30 20:11:56 +0100

    Add fps extracting function

--- video_probing.py
+++ video_probing.py
@@ -13,3 +13,18 @@ def length(path: str) -> float:
                             capture_output=True)
     length = float(status.stdout)
     return length
+
+
+def fps(path: str) -> float:
+    status = subprocess.run(["ffprobe",
+                             "-v",
+                             "error",
+                             "-show_entries",
+                             "stream=r_frame_rate",
+                             "-of",
+                             "default=noprint_wrappers=1:nokey=1",
+                             path],
+                            capture_output=True)
+    fps_str = status.stdout
+    fps = eval(fps_str)
+    return fps
