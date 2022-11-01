13708dda1ce363bd6256e4e7179f5f7340d12950
Michał Walenciak <Kicer86@gmail.com>
2022-11-01 15:50:18 +0100

    Handle vidoes with offset

--- vof_algo.py
+++ vof_algo.py
@@ -17,25 +17,32 @@ def match_frames(video1_hashes: [], video2_hashes: []) -> []:
 def adjust_videos(video1_scenes: [], video2_scenes: [],
                   video1_fps: int, video2_fps: int,
                   video1_length: float, video2_length: float) -> {}:
-    deltas = [abs(lhs_time - rhs_time) for lhs_time, rhs_time in zip(video1_scenes, video2_scenes)]
+    assert len(video1_scenes) == len(video2_scenes)
+    assert len(video1_scenes) > 1
 
-    max_delta = max(deltas)
+    deltas = [abs(lhs_time - rhs_time) for lhs_time, rhs_time in zip(video1_scenes, video2_scenes)]
+    avg_delta = sum(deltas)/len(deltas)
+    delta_diffs = [abs(delta - avg_delta) for delta in deltas]
+    max_diff = max(delta_diffs)
 
     # find what is the frame duration of video with lower fps
     min_fps = min([video1_fps, video2_fps])
     frame_duration = 1 / min_fps
 
-    # if frame_duration is bigger than biggest delta, then no work to be done
-    if frame_duration < max_delta:
+    video1_begin = video1_scenes[0] - video2_scenes[0] if video1_scenes[0] > video2_scenes[0] else 0.0
+    video2_begin = video2_scenes[0] - video1_scenes[0] if video2_scenes[0] > video1_scenes[0] else 0.0
+
+    # if frame_duration is bigger than the biggest diff, then no work to be done
+    if frame_duration < max_diff:
         pass  # for now
 
     video1_segment = {
-        "begin": 0.0,
+        "begin": video1_begin,
         "end": video1_length
     }
 
     video2_segment = {
-        "begin": 0.0,
+        "begin": video2_begin,
         "end": video2_length
     }
 
