ef957d65de20e4aa6aaa8f19f0781ed40c54cd30
Michał Walenciak <Kicer86@gmail.com>
2022-11-04 19:52:43 +0100

    Add functionf for audio codec extraction

--- mod/video_probing.py
+++ mod/video_probing.py
@@ -1,5 +1,7 @@
 
 import subprocess
+import re
+
 
 def length(path: str) -> float:
     status = subprocess.run(["ffprobe",
@@ -32,3 +34,23 @@ def fps(path: str) -> float:
     fps_line = valid_fps[0]
     fps = eval(fps_line)
     return fps
+
+
+def audio_codec(path: str) -> str:
+    process = subprocess.Popen(["ffprobe", path], stderr=subprocess.PIPE)
+
+    codec = str()
+
+    while True:
+        line_raw = process.stderr.readline()
+        if not line_raw:
+            break
+
+        line = line_raw.decode("utf-8")
+        matched = re.search("^.* Audio: ([^ ]+) .+", line)
+
+        if matched:
+            codec = matched.group(1)
+            break
+
+    return codec
