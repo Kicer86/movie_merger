15edbe754d503b83da038ad13c2e630a905d4f1b
Michał Walenciak <Kicer86@gmail.com>
2022-11-05 10:30:47 +0100

    Simplify math and test speedup video without an intro

--- mod/vof_algo.py
+++ mod/vof_algo.py
@@ -16,41 +16,27 @@ def match_frames(video1_hashes: [], video2_hashes: [], comp) -> []:
     return matches
 
 
-def are_keyframes_in_sync(video1_keyframes: [], video2_keyframes: [],
-                          video1_fps: float, video2_fps: float) -> bool:
-    assert len(video1_keyframes) == len(video2_keyframes)
-    assert len(video1_keyframes) > 1
-
-    deltas = [abs(lhs_time - rhs_time) for lhs_time, rhs_time in zip(video1_keyframes, video2_keyframes)]
-    avg_delta = sum(deltas)/len(deltas)
-    delta_diffs = [abs(delta - avg_delta) for delta in deltas]
-    max_diff = max(delta_diffs)
-
-    # find what is the frame duration of video with lower fps
-    min_fps = min([video1_fps, video2_fps])
-    frame_duration = 1 / min_fps
-
-    return frame_duration >= max_diff
-
-
 def adjust_videos(video1_keyframes: [], video2_keyframes: [],
                   video1_fps: float, video2_fps: float,
                   video1_length: float, video2_length: float) -> {}:
     assert len(video1_keyframes) == len(video2_keyframes)
     assert len(video1_keyframes) > 1
 
-    if not are_keyframes_in_sync(video1_keyframes, video2_keyframes, video1_fps, video2_fps):
-        # calculate % of diff for first and last frame
-        first_diff_percent = video1_keyframes[0] / video2_keyframes[0]
-        last_diff_percent = video1_keyframes[-1] / video2_keyframes[-1]
 
-        # apply correction for all scenes and video length
-        avg_percent = (first_diff_percent + last_diff_percent) / 2
-        video2_keyframes = [timestamp * avg_percent for timestamp in video2_keyframes]
-        video2_length *= avg_percent
+    # calculate % of diff for first and last frame
+    video1_last_first_diff = video1_keyframes[-1] - video1_keyframes[0]
+    video2_last_first_diff = video2_keyframes[-1] - video2_keyframes[0]
 
-    video1_begin = video1_keyframes[0] - video2_keyframes[0] if video1_keyframes[0] > video2_keyframes[0] else 0.0
-    video2_begin = video2_keyframes[0] - video1_keyframes[0] if video2_keyframes[0] > video1_keyframes[0] else 0.0
+    assert video1_last_first_diff > 0
+    assert video2_last_first_diff > 0
+
+    scale_factor = video1_last_first_diff / video2_last_first_diff
+
+    # apply scaling and movement:
+    # use first keyframe of first video as a origin and bas for all other points
+    video1_begin = 0.0
+    video2_begin = video1_keyframes[0] - video2_keyframes[0] * scale_factor
+    video2_length = video1_keyframes[0] + (video2_length - video2_keyframes[0]) * scale_factor
 
     video1_segment = {
         "begin": video1_begin,
