ff7997bde7bf7fa330bc329569df1d83240f725e
Michał Walenciak <Kicer86@gmail.com>
2023-02-12 21:17:59 +0100

    Test new function

--- unit_tests/vof_algo_match_frames_tests.py
+++ unit_tests/vof_algo_match_frames_tests.py
@@ -10,7 +10,7 @@ import mod.vof_algo as vof_algo
 
 class TestVOFAlgorithms(unittest.TestCase):
 
-    def test_match_frames(self):
+    def test_match_scenes(self):
         scene1 = np.array([1, 2, 3, 4])
         scene2 = np.array([2, 3, 4, 5])
         scene3 = np.array([3, 4, 5, 6])
@@ -19,11 +19,10 @@ class TestVOFAlgorithms(unittest.TestCase):
         scene6 = np.array([6, 7, 8, 9])
         scene7 = np.array([0, 1, 2, 3])
 
-        #                0       1       2       3       4       5
-        video1_scenes = [scene1, scene3, scene4, scene5, scene6]
-        video2_scenes = [scene1, scene2, scene3, scene4, scene6, scene7]
+        video1_scenes = { 0: {"hash": scene1}, 1: {"hash": scene3}, 2: {"hash": scene4}, 3: {"hash": scene5}, 4: {"hash": scene6} }
+        video2_scenes = { 0: {"hash": scene1}, 1: {"hash": scene2}, 2: {"hash": scene3}, 3: {"hash": scene4}, 4: {"hash": scene6}, 5: {"hash": scene7} }
 
-        matches = vof_algo.match_frames(video1_scenes, video2_scenes, lambda l, r: np.array_equal(l, r))
+        matches = vof_algo.match_scenes(video1_scenes, video2_scenes, lambda l, r: np.array_equal(l, r))
 
         expected_matches = [(0, 0), (1, 2), (2, 3), (4, 4)]
 
