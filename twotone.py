81afc991040371f89e2d7468aedb7a08be9a4600
Michał Walenciak <Kicer86@gmail.com>
2023-04-24 21:45:55 +0200

    Use class

--- twotone.py
+++ twotone.py
@@ -5,45 +5,50 @@ import sys
 from pathlib import Path
 
 
-def simple_subtitle_search(path: str) -> [str]:
-    video_name = Path(path).stem
-    video_extension = Path(path).suffix
-    dir = Path(path).parent
+class TwoTone:
 
-    subtitles = []
+    def __init__(self, use_mime: bool):
+        self.use_mime = use_mime
 
-    for subtitle_ext in ["txt", "srt"]:
-        subtitle_file = video_name + "." + subtitle_ext
-        subtitle_path = os.path.join(dir, subtitle_file)
-        if os.path.exists(subtitle_file):
-            subtitles.append(subtitle_file)
+    def _simple_subtitle_search(self, path: str) -> [str]:
+        video_name = Path(path).stem
+        video_extension = Path(path).suffix
+        dir = Path(path).parent
 
-    return subtitles
+        subtitles = []
 
+        for subtitle_ext in ["txt", "srt"]:
+            subtitle_file = video_name + "." + subtitle_ext
+            subtitle_path = os.path.join(dir, subtitle_file)
+            if os.path.exists(subtitle_file):
+                subtitles.append(subtitle_file)
 
-def aggressive_subtitle_search(path: str):
-    simple_subtitle_search(path)
-    pass
-
-
-def is_video(file: str):
-    mime = magic.from_file(file, mime=True)
-    return mime[:5] == "video"
+        return subtitles
 
 
-def process_dir(path: str):
-    video_files = []
-    for entry in os.scandir(path):
-        if entry.is_file() and is_video(entry.path):
-            video_files.append(entry.path)
-        elif entry.is_dir():
-            process_dir(entry.path)
+    def _aggressive_subtitle_search(self, path: str):
+        self._simple_subtitle_search(path)
+        pass
 
-    if len(video_files) == 1:
-        aggressive_subtitle_search(video_files[0])
-    if len(video_files) > 1:
-        for video_file in video_files:
-            simple_subtitle_search(video_file)
+
+    def _is_video(self, file: str):
+        mime = magic.from_file(file, mime=True)
+        return mime[:5] == "video"
+
+
+    def process_dir(self, path: str):
+        video_files = []
+        for entry in os.scandir(path):
+            if entry.is_file() and self._is_video(entry.path):
+                video_files.append(entry.path)
+            elif entry.is_dir():
+                self.process_dir(entry.path)
+
+        if len(video_files) == 1:
+            self._aggressive_subtitle_search(video_files[0])
+        if len(video_files) > 1:
+            for video_file in video_files:
+                self._simple_subtitle_search(video_file)
 
 
 if __name__ == '__main__':
@@ -51,4 +56,7 @@ if __name__ == '__main__':
         print("Provide path to scan")
         exit(1)
 
-    process_dir(sys.argv[1])
+    videos_path = sys.argv[1]
+
+    two_tone = TwoTone(False)
+    two_tone.process_dir(videos_path)
