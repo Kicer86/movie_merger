642e69b9919c1d11776e62e9d9442281d5fe70d5
Michał Walenciak <Kicer86@gmail.com>
2023-04-27 20:53:55 +0200

    Use nicer method of working directory creation

--- tests/subtitles_merge.py
+++ tests/subtitles_merge.py
@@ -9,25 +9,37 @@ import unittest
 
 import utils
 
-def prepare_temp_dir():
-    tmp_dir = "_" + inspect.stack()[1].function
-    if os.path.exists(tmp_dir):
-        shutil.rmtree(tmp_dir)
 
-    os.mkdir(tmp_dir)
+class TestDataWorkingDirectory:
+    def __init__(self):
+        self.directory = None
 
-    return tmp_dir
+    @property
+    def path(self):
+        return self.directory
+
+    def __enter__(self):
+        self.directory = "_" + inspect.stack()[1].function
+        if os.path.exists(self.directory):
+            shutil.rmtree(self.directory)
+
+        os.mkdir(self.directory)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        shutil.rmtree(self.directory)
 
 
 class SimpleSubtitlesMerge(unittest.TestCase):
 
     def test_dry_run_is_respected(self):
-        td = prepare_temp_dir()
+        with TestDataWorkingDirectory() as td:
+            for video in os.scandir("videos"):
+                if (utils.is_video(video.path, use_mime = False)):
+                    os.symlink(video, td.path + "/" + video.name)
 
-        for video in os.scandir("videos"):
-            if (utils.is_video(video.path, use_mime = False)):
-                os.symlink(video, td + "/" + video.name)
-        pass
+            for subtitle in os.scandir("subtitles"):
+                os.symlink(video, td.path + "/" + subtitle.name)
 
 
 if __name__ == '__main__':
