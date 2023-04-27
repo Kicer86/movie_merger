9ab10bfccab8bb271eaf9109e77b233214b871bd
Michał Walenciak <Kicer86@gmail.com>
2023-04-27 21:40:34 +0200

    Verify dry run does not alter files

--- tests/subtitles_merge.py
+++ tests/subtitles_merge.py
@@ -2,12 +2,31 @@
 import sys
 sys.path.append("..")
 
+import hashlib
 import inspect
 import os
 import shutil
 import unittest
 
 import utils
+import twotone
+
+
+def hashes(path: str) -> [()]:
+    results = []
+
+    for filename in os.listdir(path):
+        filepath = os.path.join(path, filename)
+
+        if os.path.isfile(filepath):
+            with open(filepath, "rb") as f:
+                file_hash = hashlib.md5()
+                while chunk := f.read(8192):
+                    file_hash.update(chunk)
+
+                results.append((filename, file_hash.hexdigest()))
+
+    return results
 
 
 class TestDataWorkingDirectory:
@@ -36,10 +55,17 @@ class SimpleSubtitlesMerge(unittest.TestCase):
         with TestDataWorkingDirectory() as td:
             for video in os.scandir("videos"):
                 if (utils.is_video(video.path, use_mime = False)):
-                    os.symlink(video, td.path + "/" + video.name)
+                    os.symlink(os.path.join(os.getcwd(), video.path), os.path.join(td.path, video.name))
 
             for subtitle in os.scandir("subtitles"):
-                os.symlink(video, td.path + "/" + subtitle.name)
+                os.symlink(os.path.join(os.getcwd(), subtitle.path), os.path.join(td.path, subtitle.name))
+
+            hashes_before = hashes(td.path)
+            self.assertTrue(len(hashes_before) == 2 * 9)        # 9 videos and 9 subtitles expected
+            twotone.run([td.path, "--dry-run"])
+            hashes_after = hashes(td.path)
+
+            self.assertEqual(hashes_before, hashes_after)
 
 
 if __name__ == '__main__':
