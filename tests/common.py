1724aad02dd3261f883a441b772fb92159fecc73
Michał Walenciak <Kicer86@gmail.com>
2023-05-10 20:11:03 +0200

    Use with for dir iteration

--- tests/common.py
+++ tests/common.py
@@ -64,12 +64,13 @@ def add_test_media(filter: str, test_case_path: str, suffixes: [str] = [None]):
     filter_regex = re.compile(filter)
 
     for media in ["subtitles", "subtitles_txt", "videos"]:
-        for file in os.scandir(os.path.join(current_path, media)):
-            file_path = file.name
-            if filter_regex.fullmatch(file_path):
-                for suffix in suffixes:
-                    suffix = "" if suffix == None else "-" + suffix + "-"
-                    file_path = Path(file)
-                    dst_file_name = file_path.stem + suffix + file_path.suffix
-                    os.symlink(os.path.join(current_path, media, file_path),
-                               os.path.join(test_case_path, dst_file_name))
+        with os.scandir(os.path.join(current_path, media)) as it:
+            for file in it:
+                file_path = file.name
+                if filter_regex.fullmatch(file_path):
+                    for suffix in suffixes:
+                        suffix = "" if suffix == None else "-" + suffix + "-"
+                        file_path = Path(file)
+                        dst_file_name = file_path.stem + suffix + file_path.suffix
+                        os.symlink(os.path.join(current_path, media, file_path),
+                                os.path.join(test_case_path, dst_file_name))
