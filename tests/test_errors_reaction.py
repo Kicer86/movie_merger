672d985a3a1c70d8b8c1584c69aa7b27bed215fa
Michał Walenciak <Kicer86@gmail.com>
2023-07-02 21:04:43 +0200

    Test patching

--- tests/test_errors_reaction.py
+++ tests/test_errors_reaction.py
@@ -8,16 +8,24 @@ from common import TestDataWorkingDirectory, list_files, add_test_media, hashes
 from unittest.mock import patch
 
 
-def start_process(cmd, args):
-    print(cmd)
-    print(args)
-
-    return utils.ProcessResult(0, b"", b"")
-
 class SimpleSubtitlesMerge(unittest.TestCase):
 
-    @patch("utils.start_process", new=start_process)
-    def test_mkvmerge_returns_with_error(self):
+    @classmethod
+    def setUpClass(cls):
+        cls._start_process = utils.start_process
+
+    @patch("utils.start_process")
+    def test_mkvmerge_returns_with_error(self, mock_start_process):
+
+        def start_process(cmd, args):
+            print(f"mocking '{cmd}' '{args}'")
+            if cmd == "ffprobe":
+                return self._start_process.__func__(cmd, args)
+            else:
+                return utils.ProcessResult(0, b"", b"")
+
+        mock_start_process.side_effect = start_process
+
         with TestDataWorkingDirectory() as td:
             add_test_media("Blue_Sky_and_Clouds_Timelapse.*(?:mov|srt)", td.path)
 
@@ -27,3 +35,7 @@ class SimpleSubtitlesMerge(unittest.TestCase):
             hashes_after = hashes(td.path)
 
             self.assertEqual(hashes_before, hashes_after)
+
+
+if __name__ == '__main__':
+    unittest.main()
