abfc96877eec102b0e74a8f8f8ab18b1e8193f6f
Michał Walenciak <Kicer86@gmail.com>
2023-07-12 22:24:54 +0200

    Test ffmpeg errors reaction

--- tests/test_errors_reaction.py
+++ tests/test_errors_reaction.py
@@ -68,5 +68,33 @@ class SimpleSubtitlesMerge(unittest.TestCase):
 
             self.assertEqual(hashes_before, hashes_after)
 
+
+    @patch("utils.start_process")
+    def test_no_changes_when_ffmpeg_exists_with_error(self, mock_start_process):
+
+        def start_process(cmd, args):
+            _, exec_name, _ = utils.split_path(cmd)
+
+            if exec_name == "ffmpeg":
+                return utils.ProcessResult(1, b"", b"")
+            else:
+                return self._start_process.__func__(cmd, args)
+
+        mock_start_process.side_effect = start_process
+
+        with TestDataWorkingDirectory() as td:
+            add_test_media("Blue_Sky_and_Clouds_Timelapse.*(?:mov|srt)", td.path)
+
+            hashes_before = hashes(td.path)
+            self.assertEqual(len(hashes_before), 2)
+            try:
+                twotone.run([td.path])
+            except RuntimeError:
+                pass
+
+            hashes_after = hashes(td.path)
+
+            self.assertEqual(hashes_before, hashes_after)
+
 if __name__ == '__main__':
     unittest.main()
