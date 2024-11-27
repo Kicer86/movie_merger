75b2bbb47731d620ab2127842938950be709598f
Michał Walenciak <michalwalenciak@gmail.com>
2024-11-27 11:54:12 +0100

    Test optimal CRF finder

---
+++ tests/test_encode.py
@@ -0,0 +1,21 @@
+
+import unittest
+import logging
+
+import encode
+from common import TestDataWorkingDirectory
+from test_utils import video_cache
+
+
+class Encode(unittest.TestCase):
+
+    def test_video_1_for_best_crf(self):
+        test_video = video_cache.fetch("video321/mp4/720/big_buck_bunny_720p_10mb.mp4")
+        best_enc = encode.find_optimal_crf(test_video, allow_segments=False)
+
+        self.assertEqual(best_enc, 29)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.ERROR)
+    unittest.main()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,17 @@ import os
 import unittest
 
 import utils
+import pooch
 from common import TestDataWorkingDirectory
+
+
+video_cache = pooch.create(
+    path=pooch.os_cache("test_data"),
+    base_url="https://www.sample-videos.com/",
+    registry={
+        "video321/mp4/720/big_buck_bunny_720p_10mb.mp4": "md5:798ce2689035bc7ed07c1f9bf75f754c",
+    },
+)
 
 
 class UtilsTests(unittest.TestCase):
