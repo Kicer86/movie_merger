5c7f6e1ca2ab6f9c605f9df082082ec95048904c
Michał Walenciak <michalwalenciak@gmail.com>
2024-12-16 20:22:47 +0100

    Turn encoder code into a class

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -2,7 +2,7 @@
 import unittest
 import logging
 
-import twotone.tools.encode as encode
+from twotone.tools.encode import Encoder
 from common import TestDataWorkingDirectory, get_video
 
 
@@ -10,15 +10,16 @@ class Encode(unittest.TestCase):
 
     def test_video_1_for_best_crf(self):
         test_video = get_video("big_buck_bunny_720p_2mb.mp4")
-        best_enc = encode.find_optimal_crf(test_video, allow_segments=False)
+        best_enc = Encoder().find_optimal_crf(test_video, allow_segments=False)
 
         self.assertEqual(best_enc, 28)
 
     def test_video_with_segments_and_no_segments(self):
+        encoder = Encoder()
         for test_video, crf in [(get_video("10189155-hd_1920_1080_25fps.mp4"), 27),
                                 (get_video("big_buck_bunny_720p_10mb.mp4"), 29)]:
-            best_enc_segments = encode.find_optimal_crf(test_video, allow_segments=True)
-            best_enc_no_segments = encode.find_optimal_crf(test_video, allow_segments=False)
+            best_enc_segments = encoder.find_optimal_crf(test_video, allow_segments=True)
+            best_enc_no_segments = encoder.find_optimal_crf(test_video, allow_segments=False)
 
             self.assertEqual(best_enc_no_segments, crf)
             self.assertTrue(abs(best_enc_no_segments - best_enc_segments) < 2)
