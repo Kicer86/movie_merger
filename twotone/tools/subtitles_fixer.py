be35809c678552ba79837953b50a3fc58b679b30
Michał Walenciak <michalwalenciak@gmail.com>
2024-12-28 10:51:21 +0100

    Drop deprecated code

--- twotone/tools/subtitles_fixer.py
+++ twotone/tools/subtitles_fixer.py
@@ -234,12 +234,3 @@ def run(args):
     fixer = Fixer(args.no_dry_run)
     fixer.process_dir(args.videos_path[0])
     logging.info("Done")
-
-
-if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-    try:
-        run(sys.argv[1:])
-    except RuntimeError as e:
-        logging.error(f"Unexpected error occurred: {e}. Terminating")
-        exit(1)
