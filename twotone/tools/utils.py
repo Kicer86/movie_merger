1878d209cc3834932e9d00a3dc0c60790bf3cbc3
Michał Walenciak <michalwalenciak@gmail.com>
2024-12-28 09:11:17 +0100

    Introduce utility for counting video frames

--- twotone/tools/utils.py
+++ twotone/tools/utils.py
@@ -171,6 +171,17 @@ def get_video_duration(video_file):
         return None
 
 
+def get_video_frames_count(video_file: str):
+    result = start_process("ffprobe", ["-v", "error", "-select_streams", "v:0", "-count_packets",
+                           "-show_entries", "stream=nb_read_packets", "-of", "csv=p=0", video_file])
+
+    try:
+        return int(result.stdout.strip())
+    except ValueError:
+        logging.error(f"Failed to get frame count for {video_file}")
+        return None
+
+
 def get_video_full_info(path: str) -> str:
     args = []
     args.extend(["-v", "quiet"])
