082038595667d52f55da98e5f1d1ac7573aa57af
Michał Walenciak <michalwalenciak@gmail.com>
2024-12-28 09:45:48 +0100

    Show progress for ffmpeg

--- twotone/tools/utils.py
+++ twotone/tools/utils.py
@@ -11,6 +11,8 @@ import sys
 from collections import namedtuple
 from itertools import islice
 from pathlib import Path
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 
 SubtitleFile = namedtuple("Subtitle", "path language encoding")
@@ -38,13 +40,35 @@ def get_tqdm_defaults():
 }
 
 
-def start_process(process: str, args: [str]) -> ProcessResult:
+def start_process(process: str, args: [str], show_progress = False) -> ProcessResult:
     command = [process]
     command.extend(args)
 
     logging.debug(f"Starting {process} with options: {' '.join(args)}")
     sub_process = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=1)
+
+    if show_progress:
+        if process == "ffmpeg":
+            index_of_i = args.index("-i")
+            input_file = args[index_of_i + 1]
+
+            if is_video(input_file):
+                progress_pattern = re.compile(r"frame= *(\d+)")
+                frames = get_video_frames_count(input_file)
+                with logging_redirect_tqdm(), \
+                     tqdm(desc="Processing video", unit="frame", total=frames, **get_tqdm_defaults()) as pbar:
+                    last_frame = 0
+                    for line in sub_process.stderr:
+                        line = line.strip()
+                        if "frame=" in line:
+                            match = progress_pattern.search(line)
+                            if match:
+                                current_frame = int(match.group(1))
+                                delta = current_frame - last_frame
+                                pbar.update(delta)
+                                last_frame = current_frame
+
     stdout, stderr = sub_process.communicate()
 
     logging.debug(f"Process finished with {sub_process.returncode}")
